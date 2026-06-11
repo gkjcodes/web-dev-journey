c55d29c54ca8f6af2a749cae3b727af994b65b95
gkjcodes <gopalkrishnjhaa@gmail.com>
2026-06-11 19:39:53 +0530

    Leart the bascis of java script, about variables, how to print and other basic operations

---
+++ java script/script.js
@@ -0,0 +1,80 @@
+let name = "Gopal";
+console.log(name);
+
+let city = "Bhagalpur";
+console.log("I live in "+ city );
+
+let course = "Web Development";
+console.log("I am currently studying "+ course );
+
+let v= 10;
+console.log(v);
+v= 20;
+console.log(v);
+v= 30;
+console.log(v);
+let firstName = "Gopal";
+let lastName = "Jha";
+let age = 18;
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+let name1 = "Rahul"
+console.log("Hello "+name1);
+
+firstName = "Aman";
+lastName = "Sharma";
+console.log(firstName + lastName);
+
+let school = "ABC School";
+console.log("My School name is "+school);
+
+
+course = "JavaScript";
+let duration = "3 months";
+
+console.log("Course= "+course);
+console.log("Duration= "+duration);
+
+
+let message = "Welcome";
+console.log(message);
+message = "Welcome Students";
+console.log(message);
+
+
+console.log("----- STUDENT INFO -----");
+console.log("Name: ___");
+console.log("Age: ___");
+console.log("Class: ___");
+console.log("------------------------");
+
+
+console.log( name1 + " is from " + city);
+
+
+let teacher = "Sonia Ma'am";
+console.log("Our teacher is"+ teacher);
+
+let institute= "Vinia";
+console.log("===== PROFILE =====");
+console.log("Name:" +name1);
+console.log("Course:"+course );
+console.log("Institute:"+institute);
+console.log("====================");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
